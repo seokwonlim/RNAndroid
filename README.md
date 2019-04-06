a5e398fa1417e1a64b60cfaf429e86fe19219801
Andrew Lim <babygom@gmail.com>
2019-04-06 23:36:32 +0900

    apply webview module

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # RNAndroid. React-Native Android
+
+
+npm(node package manager)
+https://poiemaweb.com/nodejs-npm
+
+npm install -g react@16.8.3 --save
+npm install -g react-native@0.59.3 --save
+npm install -g react-native-webview@5.6.2 --save
+
--- RNWebView/MyWeb.js
+++ RNWebView/MyWeb.js
@@ -1,14 +1,97 @@
 import React, {Component} from 'react';
-import {WebView} from 'react-native';
+import { Alert, Button, StyleSheet, Text, TextInput, 
+    TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback, 
+    View } from 'react-native';
+import {WebView} from 'react-native-webview';
 
 class MyWeb extends Component {
-  render() {
-    return (
-      <WebView
-        source={{uri: 'https://github.com/facebook/react-native'}}
-        style={{marginTop: 0}}
-      />
-    );
-  }
+    _onPressButton() {
+        Alert.alert('You tapped the button!')
+    }
+
+    _onLongPressButton() {
+        Alert.alert('You long-pressed the button!')
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {text: ''};
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.buttonContainer}>
+                    <Button
+                        onPress={this._onPressButton}
+                        title="Press Me"
+                    />
+                </View>
+                <View style={styles.buttonContainer}>
+                    <Button
+                        onPress={this._onPressButton}
+                        title="Press Me"
+                        color="#841584"
+                    />
+                </View>
+                <View style={styles.alternativeLayoutButtonContainer}>
+                    <Button
+                        onPress={this._onPressButton}
+                        title="This looks great!"
+                    />
+                    <Button
+                        onPress={this._onPressButton}
+                        title="OK!"
+                        color="#841584"
+                    />
+                </View>
+                <View style={{flex: 1, backgroundColor: 'skyblue'}}>
+                    <TextInput
+                        style={{height: 40}}
+                        placeholder="Type here to translate!"
+                        onChangeText={(text) => this.setState({text})}
+                    />
+                    <Text style={{padding: 10, fontSize: 42}}>
+                        {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+                    </Text>
+                </View>
+                <WebView
+                    source={{uri: 'https://naver.com'}}
+                    style={{flex: 1}}
+                />
+                <WebView
+                    source={{uri: 'https://google.com'}}
+                    style={{flex: 1}}
+                />
+            </View>
+        );
+    }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        //alignItems: 'center'
+    },
+    buttonContainer: {
+        margin: 20
+    },
+    button: {
+        marginBottom: 30,
+        width: 260,
+        alignItems: 'center',
+        backgroundColor: '#2196F3'
+      },
+      buttonText: {
+        padding: 20,
+        color: 'white'
+      },
+    alternativeLayoutButtonContainer: {
+        margin: 20,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    }
+  });
+
 export default MyWeb;
